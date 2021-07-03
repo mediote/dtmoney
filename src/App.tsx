ab805ada07aa57587f884e1d125c79ebbfceca21
mediote <mediote90@gmai.com>
2021-07-03 13:13:12 -0300

    context

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './contexts/TransactionsContext';
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('#root');
@@ -20,7 +21,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenTransanctionModal={handleOpenModal} />
       <Dashboard />
       <GlobalStyle />
@@ -28,6 +29,6 @@ export default function App() {
         isOpen={isModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
       />
-    </>
+    </TransactionsProvider>
   );
 }
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,25 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 
-import { api } from '../../services/api';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 import { Container } from './styles';
 
-interface Transaction {
-  id: number;
-  type: string;
-  category: string;
-  amount: number;
-  title: string;
-  createdAt: string;
-}
-
 export function TransactionsTable() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  useEffect(() => {
-    api
-      .get('transactions')
-      .then(response => setTransactions(response.data.transactions));
-  }, []);
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <Container>
